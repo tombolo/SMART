522e36ed56f261c0df33a6b040109633380a038b
Brian <ndanumumo93@gmail.com>
2025-09-04 12:29:34 +0300

    mumo

--- packages/appstore/src/components/disclaimer/disclaimer.tsx
+++ packages/appstore/src/components/disclaimer/disclaimer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text } from '@deriv/components';
 import { Localize } from '@deriv/translations';
 import { useDevice } from '@deriv-com/ui';
@@ -6,13 +6,38 @@ import './disclaimer.scss';
 
 const Disclaimer = () => {
     const { isDesktop } = useDevice();
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div data-testid='dt_traders_hub_disclaimer' className='disclaimer'>
-            <Text align='left' className='disclaimer-text' size={!isDesktop ? 'xxxs' : 'xs'}>
-                <Localize i18n_default_text='The products offered on our website are complex derivative products that carry a significant risk of potential loss. CFDs are complex instruments with a high risk of losing money rapidly due to leverage. 72% of retail investor accounts lose money when trading CFDs with this provider. You should consider whether you understand how these products work and whether you can afford to take the high risk of losing your money.' />
-            </Text>
-            <div className='disclaimer__bottom-plug' />
+        <div className="disclaimer">
+            {/* Button to open disclaimer */}
+            <button
+                className="disclaimer__button"
+                onClick={() => setIsOpen(true)}
+            >
+                Risk Disclaimer!
+            </button>
+
+            {/* Popup modal */}
+            {isOpen && (
+                <div className="disclaimer__overlay">
+                    <div className="disclaimer__popup">
+                        <button
+                            className="disclaimer__close"
+                            onClick={() => setIsOpen(false)}
+                        >
+                            ✕
+                        </button>
+                        <Text
+                            align="left"
+                            className="disclaimer__text"
+                            size={!isDesktop ? 'xxxs' : 'xs'}
+                        >
+                            <Localize i18n_default_text='The products offered on our website are complex derivative products that carry a significant risk of potential loss. CFDs are complex instruments with a high risk of losing money rapidly due to leverage. 72% of retail investor accounts lose money when trading CFDs with this provider. You should consider whether you understand how these products work and whether you can afford to take the high risk of losing your money.' />
+                        </Text>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
